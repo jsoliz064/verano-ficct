db64e8b7c8e5f49317e9f1d9509b9db437c2ccb9
jsoliz064 <jsoliz064@gmail.com>
2022-02-03 18:41:16 -0400

    user seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,7 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
         $this->call(RolesSeeder::class);
+        $this->call(UserSeeder::class);
         $this->call(CarreraSeed::class);
         $this->call(MateriaSeed::class);
         $this->call(EstudianteSeed::class);
