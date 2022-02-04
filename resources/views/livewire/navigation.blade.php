c4b8bb21f742a447d899889a5d8f66fb0f75ca16
DiegoHV <diegohurtado2412@gmail.com>
2022-02-04 12:48:19 -0400

    contact us arreglado

--- resources/views/livewire/navigation.blade.php
+++ resources/views/livewire/navigation.blade.php
@@ -2,7 +2,7 @@
     <div class="header-content">
         <div class="logo">
             <div class="logo">
-                <img src="{{ asset('imagenes/logo.png') }}" width="30"> SUPPORT
+                <img src="{{ asset('imagenes/logo.png') }}" width="30">SUPPORT
             </div>
         </div>
         <div class="menu inline-flex" id="show-menu">
@@ -17,17 +17,12 @@
                             <i class="fas fa-book"></i>Ver Materias</a>
                     </li>
                     @auth()
-<<<<<<< HEAD
-                        <li @if (Request::url() == route('estudiantes.index'))  class="menu-selected" @endif>
-                            <a @if (Request::url() != route('estudiantes.index'))  href="{{ route('estudiantes.index') }}" class="text-menu-selected text-inline" @else disabled=true class="text-inline" @endif>
-                                <i class="fas fa-book"></i>Ver Estudiantes</a>
-=======
                         <li @if (Request::url() == route('contact-us'))  class="menu-selected" @endif>
                             <a @if (Request::url() != route('contact-us'))  href="{{ route('contact-us') }}" class="text-menu-selected text-inline" @else disabled=true class="text-inline" @endif>
                                 <i class="fas fa-address-book"></i>Contáctanos</a>
->>>>>>> e957238fcdbc89b8c79156f0ae22e7d2027429f6
                         </li>
                     @endauth
+
                     @if (!auth()->user())
                         <li><a href="{{ route('login') }}" class="text-menu-selected text-inline">
                                 <i class="fas fa-user"></i>Login</a>
@@ -83,5 +78,6 @@
     <div class="container-cover">
     </div>
 </div>-->
+
 
 <script src="{{ asset('js/menu.js') }}"></script>
