7e8f3969db8e8e82d626865d40f283471f1bf025
pericena12@gmail.com <pericena12@gmail.com>
2022-02-05 15:40:43 -0400

    colores y letras

--- resources/views/livewire/formulario.blade.php
+++ resources/views/livewire/formulario.blade.php
@@ -27,108 +27,114 @@
             <form id="contact_form">
                 <!--<fieldset>-->
                 <!-- Text input-->
-                <span class="form-title">Nombre Completo</span>
-                <div class="input-group">
-                    <span class="input-group-addon" style="width: 12%;background-color:#ec0708;color:#ffff">
-                        <i class="glyphicon glyphicon-user"></i>
-                    </span>
+                <div class="form-group">
+                    <span class="form-title">Nombre Completo</span>
+                    <div class="input-group">
+                        <span class="input-group-addon" style="width: 12%;background-color:#ec0708;color:#ffff">
+                            <i class="glyphicon glyphicon-user"></i>
+                        </span>
 
-                    <input id="nombre" wire:model="nombre" name="nombre" placeholder="Nombre Completo"
-                        class="form-control2" size="10" min="10" maxlength="40" max="40" type="text" required>
-                </div>
-
-
-
-                <!-- Text input-->
-                <span class="form-title">Registro</span>
-                <div class="input-group">
-                    <span class="input-group-addon" style="width: 12%;background-color:#ec0708;color:#ffff">
-                        <i class="fas fa-layer-group"></i>
-                    </span>
-                    <input id="registro" wire:model="registro" placeholder="000000000" class="form-control2" type="tel"
-                        size="5" pattern="[0-9-+()]{4,9}" min="4" maxlength="9" max="9" required>
-                </div>
+                        <input id="nombre" wire:model="nombre" name="nombre" placeholder="Nombre Completo"
+                            class="form-control2" size="10" min="10" maxlength="40" max="40" type="text" required>
+                    </div>
 
 
 
-                <!-- Text input-->
-                <span class="form-title">Telefono</span>
-                <div class="input-group">
-                    <span class="input-group-addon" style="width: 12%;background-color:#ec0708;color:#ffff">
-                        <i class="glyphicon glyphicon-earphone"></i></span>
-                    <input id="telefono" wire:model="telefono" placeholder="+591 XXXXXXX" type="tel" size="5"
-                        pattern="[0-9-+()]{8,8}" min="8" maxlength="8" max="8" class="form-control2"
-                        style='width: 100%' required>
-                </div>
+                    <!-- Text input-->
+                    <span class="form-title">Registro</span>
+                    <div class="input-group">
+                        <span class="input-group-addon" style="width: 12%;background-color:#ec0708;color:#ffff">
+                            <i class="fas fa-layer-group"></i>
+                        </span>
+                        <input id="registro" wire:model="registro" placeholder="000000000" class="form-control2"
+                            type="tel" size="5" pattern="[0-9-+()]{4,9}" min="4" maxlength="9" max="9" required>
+                    </div>
 
 
 
-                <!-- Text input-->
-                <span class="form-title">Carrera</span>
-                <div class="input-group">
-                    <span class="input-group-addon" style="background-color:#ec0708;color:#ffff">
-                        <i class="glyphicon glyphicon-list"></i></span>
-                    <select class="form-control2 " wire:model="carrera_id" name="carrera" style='width: 100%' required>
-                        <option value="" style="background-color:#eb1b1d;color:rgb(255, 255, 255)">Seleccione una opción
-                        </option>
-                        @foreach ($carreras as $carrera)
-                            <option value="{{ $carrera->id }}"
-                                style="background-color:#eb1b1d;color:rgb(255, 255, 255)">
-                                {{ $carrera->nombre }}
+                    <!-- Text input-->
+                    <span class="form-title">Telefono</span>
+                    <div class="input-group">
+                        <span class="input-group-addon" style="width: 12%;background-color:#ec0708;color:#ffff">
+                            <i class="glyphicon glyphicon-earphone"></i></span>
+                        <input id="telefono" wire:model="telefono" placeholder="+591 XXXXXXX" type="tel" size="5"
+                            pattern="[0-9-+()]{8,8}" min="8" maxlength="8" max="8" class="form-control2"
+                            style='width: 100%' required>
+                    </div>
+
+
+
+                    <!-- Text input-->
+                    <span class="form-title">Carrera</span>
+                    <div class="input-group">
+                        <span class="input-group-addon" style="background-color:#ec0708;color:#ffff">
+                            <i class="glyphicon glyphicon-list"></i></span>
+                        <select class="form-control2 " wire:model="carrera_id" name="carrera" style='width: 100%'
+                            required>
+                            <option value="" style="background-color:#eb1b1d;color:rgb(255, 255, 255)">Seleccione una
+                                opción
                             </option>
-                        @endforeach
-                    </select>
+                            @foreach ($carreras as $carrera)
+                                <option value="{{ $carrera->id }}"
+                                    style="background-color:#eb1b1d;color:rgb(255, 255, 255)">
+                                    {{ $carrera->nombre }}
+                                </option>
+                            @endforeach
+                        </select>
+                    </div>
+
+
+
+                    <!-- Text input-->
+
+
+                    <!-- Select Basic -->
+                    <span class="form-title">Materia 1</span>
+                    <div class="mb-4 w-full inline-flex" wire:ignore>
+                        <span class="input-group-addon pr-5" style="width: 12%;background-color:#ec0708;color:#ffff">
+                            <i class="glyphicon glyphicon-list"></i></span>
+                        <select wire:model='materia1' class="idMateria1" name="materia1" style='width: 100%'
+                            required>
+                            <option value="">Seleccione una materia</option>
+                            @foreach ($materias as $materia)
+                                <option value="{{ $materia->id }}">{{ $materia->nombre }} </option>
+                            @endforeach
+                        </select>
+                    </div>
+
+
+
+
+                    <!-- Text input-->
+                    <!-- Select Basic -->
+                    <span class="form-title">Materia 2</span>
+                    <div class="mb-4 w-full inline-flex" wire:ignore>
+                        <span class="input-group-addon pr-5" style="width: 12%;background-color:#ec0708;color:#ffff">
+                            <i class="glyphicon glyphicon-list"></i></span>
+                        <select wire:model='materia2' class="idMateria2" name="materia2" style='width: 100%'
+                            required>
+                            <option value="">Selecciona una materia</option>
+                            @foreach ($materias as $materia)
+                                <option value="{{ $materia->id }}">{{ $materia->nombre }} </option>
+                            @endforeach
+                        </select>
+                    </div>
+
+
+
+                    <!-- Button -->
+                    <div>
+                        <button wire:click.prevent="guardar()" type="button" class="btn-login">Inscribir
+                        </button>
+                        @if (session()->has('message'))
+                            <h4>{{ session('message') }}</h4>
+                        @endif
+                    </div>
+                    <center>
+                        <h5>Facultad
+                            de Ingeniera en Ciencias de la Computación.</h5>
+                    </center>
                 </div>
-
-
-
-                <!-- Text input-->
-
-
-                <!-- Select Basic -->
-                <span class="form-title">Materia 1</span>
-                <div class="mb-4 w-full inline-flex" wire:ignore>
-                    <span class="input-group-addon pr-5" style="width: 12%;background-color:#ec0708;color:#ffff">
-                        <i class="glyphicon glyphicon-list"></i></span>
-                    <select wire:model='materia1' class="idMateria1" name="materia1" style='width: 100%' required>
-                        <option value="">Seleccione una materia</option>
-                        @foreach ($materias as $materia)
-                            <option value="{{ $materia->id }}">{{ $materia->nombre }} </option>
-                        @endforeach
-                    </select>
-                </div>
-
-
-
-
-                <!-- Text input-->
-                <!-- Select Basic -->
-                <span class="form-title">Materia 2</span>
-                <div class="mb-4 w-full inline-flex" wire:ignore>
-                    <span class="input-group-addon pr-5" style="width: 12%;background-color:#ec0708;color:#ffff">
-                        <i class="glyphicon glyphicon-list"></i></span>
-                    <select wire:model='materia2' class="idMateria2" name="materia2" style='width: 100%' required>
-                        <option value="">Selecciona una materia</option>
-                        @foreach ($materias as $materia)
-                            <option value="{{ $materia->id }}">{{ $materia->nombre }} </option>
-                        @endforeach
-                    </select>
-                </div>
-
-
-
-                <!-- Button -->
-                <div>
-                    <button wire:click.prevent="guardar()" type="button" class="btn-login">Inscribir
-                    </button>
-                    @if (session()->has('message'))
-                        <h4>{{ session('message') }}</h4>
-                    @endif
-                </div>
-                <center>
-                    <h5>Facultad
-                        de Ingeniera en Ciencias de la Computación.</h5>
-                </center>
             </form>
         </div>
     </div>
