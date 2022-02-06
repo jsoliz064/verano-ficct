e8636281e4b903342396d6cb8c69326392fa7495
jsoliz064 <jsoliz064@gmail.com>
2022-02-06 01:28:40 -0400

    cache

---
+++ bootstrap/cache/livewire-components.php
@@ -0,0 +1,8 @@
+<?php return array (
+  'estudiantes' => 'App\\Http\\Livewire\\Estudiantes',
+  'estudiantes-admin' => 'App\\Http\\Livewire\\EstudiantesAdmin',
+  'formulario' => 'App\\Http\\Livewire\\Formulario',
+  'materias' => 'App\\Http\\Livewire\\Materias',
+  'navigation' => 'App\\Http\\Livewire\\Navigation',
+  'show-materia' => 'App\\Http\\Livewire\\ShowMateria',
+);
