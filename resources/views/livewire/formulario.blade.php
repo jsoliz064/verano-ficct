8373c5f8affacc2f00249bd732c9c029adc58587
jsoliz064 <jsoliz064@gmail.com>
2022-02-07 22:25:43 -0400

    gaaa

--- resources/views/livewire/formulario.blade.php
+++ resources/views/livewire/formulario.blade.php
@@ -225,3 +225,4 @@
 <script src="{{ asset('js/formulario.js') }}"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.6-rc.0/js/select2.min.js"></script>
 <script src="{{ asset('js/mi-script.js') }}"></script>
+@livewireScripts
